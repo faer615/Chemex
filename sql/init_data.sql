912402d9591573c1408c73a00876c1cb28946cb0
Famio <famio@qq.com>
2020-09-28 15:55:43 +0800

    :construction: 更新SQL脚本

--- sql/init_data.sql
+++ sql/init_data.sql
@@ -76,22 +76,23 @@ INSERT INTO `admin_users` (`id`, `username`, `password`, `name`, `avatar`, `reme
 
 DELETE FROM `device_categories`;
 /*!40000 ALTER TABLE `device_categories` DISABLE KEYS */;
-INSERT INTO `device_categories` (`id`, `name`, `description`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, '笔记本', NULL, NULL, '2020-09-24 08:28:32', '2020-09-24 08:28:32'),
-	(2, '台式机', NULL, NULL, '2020-09-24 08:28:36', '2020-09-24 08:28:36');
+INSERT INTO `device_categories` (`id`, `name`, `description`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, '笔记本', NULL, NULL, NULL, '2020-09-24 08:28:32', '2020-09-24 08:28:32'),
+	(2, '台式机', NULL, NULL, NULL, '2020-09-24 08:28:36', '2020-09-24 08:28:36');
 /*!40000 ALTER TABLE `device_categories` ENABLE KEYS */;
 
 DELETE FROM `device_records`;
 /*!40000 ALTER TABLE `device_records` DISABLE KEYS */;
-INSERT INTO `device_records` (`id`, `name`, `description`, `category_id`, `vendor_id`, `sn`, `mac`, `ip`, `photo`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, 'PC-00001', NULL, 2, 2, 'DDD-A12319231', NULL, '192.168.1.12', 'images/6715ef80607acca796aaafd6a8136028.jpg', NULL, '2020-09-24 08:29:49', '2020-09-28 08:10:42'),
-	(2, 'SERVER-A1', NULL, 2, 2, 'DDD-A12319231', 'FF-FF-FF-FF-FF-FF', '192.168.1.2', NULL, NULL, '2020-09-24 14:28:46', '2020-09-24 14:28:46');
+INSERT INTO `device_records` (`id`, `name`, `description`, `category_id`, `vendor_id`, `sn`, `mac`, `ip`, `photo`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, 'PC-00001', NULL, 2, 2, 'DDD-A12319231', NULL, '192.168.1.12', 'images/6715ef80607acca796aaafd6a8136028.jpg', 'Administrator', NULL, '2020-09-24 08:29:49', '2020-09-28 15:04:32'),
+	(2, 'SERVER-A1', NULL, 2, 2, 'DDD-A12319231', 'FF-FF-FF-FF-FF-FF', '192.168.1.2', NULL, NULL, NULL, '2020-09-24 14:28:46', '2020-09-24 14:28:46'),
+	(3, 'FAMIO-X', NULL, 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, '2020-09-28 14:35:55', '2020-09-28 14:35:55');
 /*!40000 ALTER TABLE `device_records` ENABLE KEYS */;
 
 DELETE FROM `device_tracks`;
 /*!40000 ALTER TABLE `device_tracks` DISABLE KEYS */;
-INSERT INTO `device_tracks` (`id`, `device_id`, `staff_id`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, 1, 1, NULL, '2020-09-24 08:37:57', '2020-09-24 08:37:57');
+INSERT INTO `device_tracks` (`id`, `device_id`, `staff_id`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, 1, 1, '', NULL, '2020-09-24 08:37:57', '2020-09-24 08:37:57');
 /*!40000 ALTER TABLE `device_tracks` ENABLE KEYS */;
 
 DELETE FROM `failed_jobs`;
@@ -100,69 +101,69 @@ DELETE FROM `failed_jobs`;
 
 DELETE FROM `hardware_categories`;
 /*!40000 ALTER TABLE `hardware_categories` DISABLE KEYS */;
-INSERT INTO `hardware_categories` (`id`, `name`, `description`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, '固态硬盘', NULL, NULL, '2020-09-24 08:19:43', '2020-09-24 08:19:43'),
-	(2, '内存', NULL, NULL, '2020-09-24 08:19:53', '2020-09-24 08:19:53');
+INSERT INTO `hardware_categories` (`id`, `name`, `description`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, '固态硬盘', NULL, NULL, NULL, '2020-09-24 08:19:43', '2020-09-24 08:19:43'),
+	(2, '内存', NULL, NULL, NULL, '2020-09-24 08:19:53', '2020-09-24 08:19:53');
 /*!40000 ALTER TABLE `hardware_categories` ENABLE KEYS */;
 
 DELETE FROM `hardware_records`;
 /*!40000 ALTER TABLE `hardware_records` DISABLE KEYS */;
-INSERT INTO `hardware_records` (`id`, `name`, `description`, `category_id`, `vendor_id`, `specification`, `sn`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, 'Fury', NULL, 2, 4, 'DDR4 2400', NULL, NULL, '2020-09-24 08:23:34', '2020-09-24 09:18:59'),
-	(2, 'SN720', NULL, 1, 2, '500G', NULL, NULL, '2020-09-24 08:26:02', '2020-09-24 08:26:02'),
-	(3, 'Fury', NULL, 2, 4, 'DDR4 2400', NULL, NULL, '2020-09-24 09:19:46', '2020-09-24 09:19:46');
+INSERT INTO `hardware_records` (`id`, `name`, `description`, `category_id`, `vendor_id`, `specification`, `sn`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, 'Fury', NULL, 2, 4, 'DDR4 2400', NULL, NULL, NULL, '2020-09-24 08:23:34', '2020-09-24 09:18:59'),
+	(2, 'SN720', NULL, 1, 2, '500G', NULL, NULL, NULL, '2020-09-24 08:26:02', '2020-09-24 08:26:02'),
+	(3, 'Fury', NULL, 2, 4, 'DDR4 2400', NULL, NULL, NULL, '2020-09-24 09:19:46', '2020-09-24 09:19:46');
 /*!40000 ALTER TABLE `hardware_records` ENABLE KEYS */;
 
 DELETE FROM `hardware_tracks`;
 /*!40000 ALTER TABLE `hardware_tracks` DISABLE KEYS */;
-INSERT INTO `hardware_tracks` (`id`, `hardware_id`, `device_id`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, 1, 1, '2020-09-27 13:38:02', '2020-09-24 08:47:30', '2020-09-24 08:47:30'),
-	(2, 2, 1, NULL, '2020-09-24 08:47:42', '2020-09-24 08:47:42'),
-	(3, 3, 1, NULL, '2020-09-24 09:20:01', '2020-09-24 09:20:01');
+INSERT INTO `hardware_tracks` (`id`, `hardware_id`, `device_id`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, 1, 1, NULL, '2020-09-27 13:38:02', '2020-09-24 08:47:30', '2020-09-24 08:47:30'),
+	(2, 2, 1, NULL, NULL, '2020-09-24 08:47:42', '2020-09-24 08:47:42'),
+	(3, 3, 1, NULL, NULL, '2020-09-24 09:20:01', '2020-09-24 09:20:01');
 /*!40000 ALTER TABLE `hardware_tracks` ENABLE KEYS */;
 
 DELETE FROM `software_categories`;
 /*!40000 ALTER TABLE `software_categories` DISABLE KEYS */;
-INSERT INTO `software_categories` (`id`, `name`, `description`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, '办公应用', NULL, NULL, '2020-09-24 08:27:04', '2020-09-24 08:27:04'),
-	(2, '操作系统', NULL, NULL, '2020-09-24 08:27:09', '2020-09-24 08:27:09');
+INSERT INTO `software_categories` (`id`, `name`, `description`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, '办公应用', NULL, NULL, NULL, '2020-09-24 08:27:04', '2020-09-24 08:27:04'),
+	(2, '操作系统', NULL, NULL, NULL, '2020-09-24 08:27:09', '2020-09-24 08:27:09');
 /*!40000 ALTER TABLE `software_categories` ENABLE KEYS */;
 
 DELETE FROM `software_records`;
 /*!40000 ALTER TABLE `software_records` DISABLE KEYS */;
-INSERT INTO `software_records` (`id`, `name`, `description`, `category_id`, `version`, `vendor_id`, `price`, `purchased`, `expired`, `distribution`, `sn`, `counts`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, 'Office 2019', NULL, 1, 'Business', 1, 1500, '2020-09-24', '2020-09-24', 'b', NULL, 5, NULL, '2020-09-24 08:27:44', '2020-09-24 08:27:44'),
-	(2, 'Windows 10', NULL, 2, 'Pro', 1, 2000, '2020-09-24', '2020-09-24', 'b', NULL, 1, NULL, '2020-09-24 08:28:09', '2020-09-24 08:28:09');
+INSERT INTO `software_records` (`id`, `name`, `description`, `category_id`, `version`, `vendor_id`, `price`, `purchased`, `expired`, `distribution`, `sn`, `counts`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, 'Office 2019', NULL, 1, 'Business', 1, 1500, '2020-09-24', '2020-09-24', 'b', NULL, 5, '1', NULL, '2020-09-24 08:27:44', '2020-09-24 08:27:44'),
+	(2, 'Windows 10', NULL, 2, 'Pro', 1, 2000, '2020-09-24', '2020-09-24', 'b', NULL, 1, '1', NULL, '2020-09-24 08:28:09', '2020-09-24 08:28:09');
 /*!40000 ALTER TABLE `software_records` ENABLE KEYS */;
 
 DELETE FROM `software_tracks`;
 /*!40000 ALTER TABLE `software_tracks` DISABLE KEYS */;
-INSERT INTO `software_tracks` (`id`, `software_id`, `device_id`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, 1, 1, '2020-09-27 14:44:31', '2020-09-24 08:33:59', '2020-09-24 08:33:59'),
-	(2, 2, 1, NULL, '2020-09-24 08:59:42', '2020-09-24 08:59:42');
+INSERT INTO `software_tracks` (`id`, `software_id`, `device_id`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, 1, 1, NULL, '2020-09-27 14:44:31', '2020-09-24 08:33:59', '2020-09-24 08:33:59'),
+	(2, 2, 1, NULL, NULL, '2020-09-24 08:59:42', '2020-09-24 08:59:42');
 /*!40000 ALTER TABLE `software_tracks` ENABLE KEYS */;
 
 DELETE FROM `staff_departments`;
 /*!40000 ALTER TABLE `staff_departments` DISABLE KEYS */;
-INSERT INTO `staff_departments` (`id`, `name`, `description`, `parent_id`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, '行政部', NULL, NULL, NULL, '2020-09-24 08:37:11', '2020-09-24 08:37:11'),
-	(2, '财务部', NULL, NULL, NULL, '2020-09-24 08:37:16', '2020-09-24 08:37:16');
+INSERT INTO `staff_departments` (`id`, `name`, `description`, `parent_id`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, '行政部', NULL, NULL, NULL, NULL, '2020-09-24 08:37:11', '2020-09-24 08:37:11'),
+	(2, '财务部', NULL, NULL, NULL, NULL, '2020-09-24 08:37:16', '2020-09-24 08:37:16');
 /*!40000 ALTER TABLE `staff_departments` ENABLE KEYS */;
 
 DELETE FROM `staff_records`;
 /*!40000 ALTER TABLE `staff_records` DISABLE KEYS */;
-INSERT INTO `staff_records` (`id`, `name`, `department_id`, `gender`, `title`, `mobile`, `email`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, '张三', 1, '男', NULL, NULL, NULL, NULL, '2020-09-24 08:37:50', '2020-09-24 08:37:50');
+INSERT INTO `staff_records` (`id`, `name`, `department_id`, `gender`, `title`, `mobile`, `email`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, '张三', 1, '男', NULL, NULL, NULL, NULL, NULL, '2020-09-24 08:37:50', '2020-09-24 08:37:50');
 /*!40000 ALTER TABLE `staff_records` ENABLE KEYS */;
 
 DELETE FROM `vendor_records`;
 /*!40000 ALTER TABLE `vendor_records` DISABLE KEYS */;
-INSERT INTO `vendor_records` (`id`, `name`, `description`, `location`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(1, '微软', '计算机软硬件制造商', '美国', NULL, '2020-09-24 08:19:03', '2020-09-24 08:19:03'),
-	(2, 'DELL', '计算机硬件制造商', '美国', NULL, '2020-09-24 08:19:12', '2020-09-24 08:19:12'),
-	(3, 'Adobe', NULL, NULL, NULL, '2020-09-24 09:17:56', '2020-09-24 09:17:56'),
-	(4, '金士顿', NULL, NULL, NULL, '2020-09-24 09:18:03', '2020-09-24 09:18:03'),
-	(5, '西部数据', NULL, NULL, NULL, '2020-09-24 09:18:13', '2020-09-24 09:18:13');
+INSERT INTO `vendor_records` (`id`, `name`, `description`, `location`, `creator`, `deleted_at`, `created_at`, `updated_at`) VALUES
+	(1, '微软', '计算机软硬件制造商', '美国', NULL, NULL, '2020-09-24 08:19:03', '2020-09-24 08:19:03'),
+	(2, 'DELL', '计算机硬件制造商', '美国', NULL, NULL, '2020-09-24 08:19:12', '2020-09-24 08:19:12'),
+	(3, 'Adobe', NULL, NULL, NULL, NULL, '2020-09-24 09:17:56', '2020-09-24 09:17:56'),
+	(4, '金士顿', NULL, NULL, NULL, NULL, '2020-09-24 09:18:03', '2020-09-24 09:18:03'),
+	(5, '西部数据', NULL, NULL, NULL, NULL, '2020-09-24 09:18:13', '2020-09-24 09:18:13');
 /*!40000 ALTER TABLE `vendor_records` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
