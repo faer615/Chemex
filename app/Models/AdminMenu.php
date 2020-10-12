e89f9bf27b64c42bb2943ebb27a42cb91790675c
celaraze <celaraze@qq.com>
2020-10-12 19:33:23 +0800

    🚧 优化路由&禁止在演示模式下修改部分内容

--- app/Models/AdminMenu.php
+++ app/Models/AdminMenu.php
@@ -2,15 +2,11 @@
 
 namespace App\Models;
 
-use Dcat\Admin\Admin;
 use Dcat\Admin\Traits\HasDateTimeFormatter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @method static where(string $key, string $value)
- */
-class AdminUser extends Model
+class AdminMenu extends Model
 {
     use HasFactory;
     use HasDateTimeFormatter;
