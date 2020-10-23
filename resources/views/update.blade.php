b431f3133c411f5a4a5c32a3c16646fe89c95ca1
celaraze <celaraze@qq.com>
2020-10-23 15:07:19 +0800

    🔧 测试自动更新

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,5 +1,23 @@
 <div>当前版本：{{$data['old']}}</div>
 <div class="mt-1">最新版本：{{$data['new']}}</div>
 @if($data['res'] == -1)
-    <a href="{{$data['url']}}" target="_blank" style="width: 100%;" class="btn btn-primary mt-1">下载新版本</a>
+    <button id="update" style="width: 100%;" class="btn btn-primary mt-1" onclick="unzip('{{$data['url']}}')">更新
+    </button>
 @endif
+<script>
+    function unzip(url) {
+        document.getElementById('update').setAttribute('disabled', 'disabled');
+        document.getElementById('update').innerText = '正在更新';
+        $.ajax({
+            url: '/unzip?url=' + url,
+            type: 'get',
+            success: function () {
+                window.location.reload();
+            },
+            complete: function () {
+                document.getElementById('update').setAttribute('disabled', '');
+                document.getElementById('update').innerText = '更新';
+            }
+        });
+    }
+</script>
