1b3e3bdb531b48116949b56db38c3f34398c5ca5
Celaraze <celaraze@qq.com>
2020-10-25 10:05:02 +0800

    🚧 接口测试：鉴权相关完成

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,25 +6,13 @@ use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     * 要求附带email和password（数据来源users表）
-     *
-     * @return void
-     */
     public function __construct()
     {
-        // 这里额外注意了：官方文档样例中只除外了『login』
-        // 这样的结果是，token 只能在有效期以内进行刷新，过期无法刷新
-        // 如果把 refresh 也放进去，token 即使过期但仍在刷新期以内也可刷新
-        // 不过刷新一次作废
-        $this->middleware('auth:api', ['except' => ['login']]);
-        // 另外关于上面的中间件，官方文档写的是『auth:api』
-        // 但是我推荐用 『jwt.auth』，效果是一样的，但是有更加丰富的报错信息返回
+        $this->middleware('jwt.auth', ['except' => ['login']]);
     }
 
     /**
-     * Get a JWT via given credentials.
+     * 登录
      *
      * @return JsonResponse
      */
@@ -40,7 +28,7 @@ class AuthController extends Controller
     }
 
     /**
-     * Get the token array structure.
+     * 返回Token
      *
      * @param string $token
      *
@@ -55,7 +43,7 @@ class AuthController extends Controller
     }
 
     /**
-     * Get the authenticated User.
+     * 获取登录的用户信息
      *
      * @return JsonResponse
      */
@@ -65,7 +53,7 @@ class AuthController extends Controller
     }
 
     /**
-     * Log the user out (Invalidate the token).
+     * 注销
      *
      * @return JsonResponse
      */
@@ -77,7 +65,6 @@ class AuthController extends Controller
     }
 
     /**
-     * Refresh a token.
      * 刷新token，如果开启黑名单，以前的token便会失效。
      * 值得注意的是用上面的getToken再获取一次Token并不算做刷新，两次获得的Token是并行的，即两个都可用。
      * @return JsonResponse
