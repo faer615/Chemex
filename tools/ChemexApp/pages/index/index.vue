599371d18f10af1c982a651f0108f17c6501d9f9
Celaraze <celaraze@qq.com>
2020-10-25 10:10:17 +0800

    增加移动端项目

---
+++ tools/ChemexApp/pages/index/index.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="mode">
+			<view style="background-color: rgba(99,181,247,1);" @click="toQuery()">查询模式</view>
+			<view style="background-color: rgba(76,181,171,1);" @click="toCheck()">盘点模式</view>
+			<view style="background-color: rgba(255,153,76,1);" @click="toSetting()">站点配置</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			toQuery() {
+				uni.navigateTo({
+					url: '../query/query'
+				})
+			},
+			toCheck() {
+				uni.navigateTo({
+					url: '../check/check'
+				})
+			},
+			toSetting() {
+				uni.navigateTo({
+					url: '../setting/setting'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.mode {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.mode>view {
+		color: white;
+		width: 400upx;
+		height: 150upx;
+		margin-top: 50upx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 40upx;
+		border-radius: 20upx;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ tools/ChemexApp/pages/query/query.vue
@@ -0,0 +1,48 @@
+<template>
+	<view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: '',
+				config: ''
+			}
+		},
+		onLoad() {
+			let that = this;
+			uni.getStorage({
+				key: 'config',
+				success(res) {
+					that.config = res.data;
+				},
+				fail() {
+					uni.showModal({
+						title: '提示',
+						content: '没有找到配置信息'
+					})
+				}
+			})
+		},
+		methods: {
+			query() {
+
+			},
+			scan() {
+				uni.scanCode({
+					success: function(res) {
+						console.log('条码类型：' + res.scanType);
+						console.log('条码内容：' + res.result);
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
