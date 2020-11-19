52f0348d9fc9d88546696cc3b36e1fa4ba4ac31e
Celaraze <celaraze@qq.com>
2020-11-19 11:05:13 +0800

    修复：制造商管理联系人可为NULL

--- database/migrations/2020_11_18_155729_update_vendor_records_columns_table.php
+++ database/migrations/2020_11_18_155729_update_vendor_records_columns_table.php
@@ -14,7 +14,7 @@ class UpdateVendorRecordsColumnsTable extends Migration
     public function up()
     {
         Schema::table('vendor_records', function (Blueprint $table) {
-            $table->json('contacts');
+            $table->json('contacts')->nullable();
         });
     }
 
